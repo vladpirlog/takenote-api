a253a7cba355c45ff2df215955e07d6558910d5d
Vlad PIRLOG <avi.pirlog@gmail.com>
2020-07-10 21:05:21 +0300

    Rename function and file

--- src/middlewares/extractUserFromCookie.middleware.ts
+++ src/middlewares/extractUserFromCookie.middleware.ts
@@ -5,14 +5,12 @@ import authJWT from '../utils/authJWT.util'
  * Middleware function that identifies the user making the request based on the JWT cookie.
  * It saves user info in res.locals.user (userID, role, state).
  */
-export default async function getLoggedUser (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
+const extractUserFromCookie = async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.locals.user = req.cookies.access_token
             ? await authJWT.verify(req.cookies.access_token) : null
         return next()
     } catch (err) { return next(err) }
 }
+
+export default extractUserFromCookie
