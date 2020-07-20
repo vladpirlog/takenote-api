15fe43a69463f95c1f6de693b0d8d2e0cce6c5e2
Vlad PIRLOG <avi.pirlog@gmail.com>
2020-07-20 21:27:08 +0300

    :recycle: Rewrite rate limiting middleware

--- src/middlewares/rateLimiting.middleware.ts
+++ src/middlewares/rateLimiting.middleware.ts
@@ -4,68 +4,37 @@ import { IRateLimiting } from '../interfaces/rateLimiting.interface'
 import getUnixTime from '../utils/getUnixTime.util'
 import constants from '../config/constants.config'
 import redisConfig from '../config/redis.config'
-import { RedisClient } from 'redis'
-
-const setRateLimitHeaders = (res: Response, data: IRateLimiting) => {
-    const remainingRequests = constants.rateLimiting.request - data.request.counter
-    res.setHeader(
-        'X-RateLimit-Remaining-Minute',
-        remainingRequests < 0 ? 0 : remainingRequests
-    )
-    res.setHeader('X-RateLimit-Limit-Minute', constants.rateLimiting.request)
-    return res
-}
-
-const createNewEntry = (
-    request: IRateLimiting['request']['counter'] | IRateLimiting['request'],
-    email: IRateLimiting['email']['counter'] | IRateLimiting['email']
-): IRateLimiting => {
-    return {
-        request: typeof request === 'number'
-            ? {
-                counter: request, unixTime: getUnixTime()
-            } : request,
-        email: typeof email === 'number'
-            ? {
-                counter: email, unixTime: getUnixTime()
-            } : email
-    }
-}
-
-const saveAndSetHeaders = (
-    res: Response,
-    redisClient: RedisClient,
-    data: IRateLimiting,
-    ipAddr: string,
-    type: 'request' | 'email'
-) => {
-    redisClient.setex(ipAddr, 60, JSON.stringify(data))
-    return type === 'request' ? setRateLimitHeaders(res, data) : res
-}
 
 const rateLimiting = (type: 'request' | 'email') => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
+            const key = `${req.ip}--${type}`
             const redisClient = await redisConfig.getClient()
             const reply: string = await new Promise((resolve, reject) => {
-                redisClient.get(req.ip, (err, reply) => err ? reject(err) : resolve(reply))
+                redisClient.get(key, (err, reply) => err ? reject(err) : resolve(reply))
             })
             let data: IRateLimiting
+            let blockRequest = false
+
             if (reply) {
                 data = JSON.parse(reply)
-                if (data[type].counter >= constants.rateLimiting[type]) {
-                    if (getUnixTime() - data[type].unixTime < 60) {
-                        data[type].counter++
-                        res = saveAndSetHeaders(res, redisClient, data, req.ip, type)
-                        return createResponse(res, 429)
-                    } else {
-                        data = createNewEntry(type === 'request' ? 1 : data.request,
-                            type === 'email' ? 1 : data.email)
-                    }
-                } else { data[type].counter++ }
-            } else { data = createNewEntry(type === 'request' ? 1 : 0, type === 'email' ? 1 : 0) }
-            res = saveAndSetHeaders(res, redisClient, data, req.ip, type)
-            return next()
+                if (data.counter >= constants.rateLimiting[type]) {
+                    if (getUnixTime() - data.unixTime < 60) {
+                        data.counter++
+                        blockRequest = true
+                    } else { data = { counter: 1, unixTime: getUnixTime() } }
+                } else { data.counter++ }
+            } else { data = { counter: 1, unixTime: getUnixTime() } }
+
+            redisClient.setex(key, 60, JSON.stringify(data))
+            if (type === 'request') {
+                res.setHeader(
+                    'X-RateLimit-Remaining-Minute',
+                    Math.max(0, constants.rateLimiting.request - data.counter)
+                )
+                res.setHeader('X-RateLimit-Limit-Minute', constants.rateLimiting.request)
+            }
+            return blockRequest ? createResponse(res, 429) : next()
         } catch (err) { return next(err) }
     }
 }
