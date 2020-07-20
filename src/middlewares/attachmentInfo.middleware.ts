dd9ab6dece2396a35d1380ec27b56cdc97013507
Vlad PIRLOG <avi.pirlog@gmail.com>
2020-07-20 21:22:09 +0300

    :recycle: Add delete file function

--- src/middlewares/attachmentInfo.middleware.ts
+++ src/middlewares/attachmentInfo.middleware.ts
@@ -1,32 +1,30 @@
 import { Request, Response, NextFunction } from 'express'
 import createResponse from '../utils/createResponse.util'
 import { UploadedFile } from 'express-fileupload'
-import fs from 'fs'
+import deleteFile from '../utils/deleteFile.util'
 
 /**
  * Middleware function that checks if the attachment photo exists in the request and if it meets the size and mimetype requirements
  */
-export default function checkAttachmentInfo (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
+const checkAttachmentInfo = (req: Request, res: Response, next: NextFunction) => {
     try {
         if (!req.files || !req.files.photo) { return createResponse(res, 404, 'File not found.') }
         const file: UploadedFile | UploadedFile[] = req.files.photo
         if (Array.isArray(file)) {
-            file.forEach((f) => fs.unlinkSync(f.tempFilePath))
+            deleteFile(file)
             return createResponse(res, 400, 'Multiple files uploaded.')
         }
         if (!['image/jpeg', 'image/png'].includes(file.mimetype)) {
-            fs.unlinkSync(file.tempFilePath)
+            deleteFile(file)
             return createResponse(res, 415, 'File is not jpeg or png.')
         }
 
         if (file.size > 8000000) {
-            fs.unlinkSync(file.tempFilePath)
+            deleteFile(file)
             return createResponse(res, 413, 'File is larger than 8 MB.')
         }
         return next()
     } catch (err) { return next(err) }
 }
+
+export default checkAttachmentInfo
