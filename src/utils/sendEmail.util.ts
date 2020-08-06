2bb7c8c316690e486eb535cd41deacb0ef98117b
Vlad Pirlog <avi.pirlog@gmail.com>
2020-08-06 21:49:16 +0300

    :recycle: Refactor sendEmail function

--- src/utils/sendEmail.util.ts
+++ src/utils/sendEmail.util.ts
@@ -13,51 +13,22 @@ const sendToken = async (
     user: IUserSchema,
     type: 'reset' | 'forgot' | 'confirmation'
 ) => {
-    let info: {token: string, pathname: string}
-    if (type === 'reset') info = { token: user.resetToken.id, pathname: '/rpassword' }
-    else if (type === 'forgot') info = { token: user.forgotToken.id, pathname: '/fpassword' }
-    else info = { token: user.confirmationToken.id, pathname: '/confirm' }
+    const data = {
+        reset: { token: user?.resetToken?.id, path: '/rpassword' },
+        forgot: { token: user?.forgotToken?.id, path: '/fpassword' },
+        confirmation: { token: user?.confirmationToken?.id, path: '/confirm' }
+    }
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        secure: false,
-        auth: {
-            user: constants.email.user,
-            pass: constants.email.pass
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+    const completeURL = makeCompleteURL(data[type])
+    const tokenMailContent = makeTokenMailContent(
+        user,
+        completeURL,
+        type === 'confirmation'
+    )
+    const mailOptions = makeMailOptions(user, tokenMailContent)
+    const mailTransport = makeMailTransport()
 
-    const completeURL = url.format({
-        query: {
-            token: info.token
-        },
-        protocol: 'http', // TODO: change to https
-        host: constants.domain.baseDomain,
-        pathname: info.pathname
-    })
-
-    const emailSubject: string = type === 'confirmation'
-        ? `TakeNote Account Confirmation - ${user.username}`
-        : `TakeNote Account Password Reset - ${user.username}`
-
-    const emailText: string = type === 'confirmation'
-        ? `Go to this URL to confirm your account: ${completeURL}`
-        : `Go to this URL to reset your password: ${completeURL}`
-
-    const emailHTML : string = type === 'confirmation'
-        ? `<h3>Click the button below to confirm your TakeNote account</h3><button><a href=${completeURL} target="_blank">Confirm</a></button><p>Button not working? Go to this URL: ${completeURL}</p>`
-        : `<h3>Click the button below to reset your TakeNote password</h3><button><a href=${completeURL} target="_blank">Reset Password</a></button><p>Button not working? Go to this URL: ${completeURL}</p>`
-
-    await transporter.sendMail({
-        from: `"TakeNote" ${constants.email.user}`,
-        to: user.email,
-        subject: emailSubject,
-        text: emailText,
-        html: emailHTML
-    })
+    await mailTransport.sendMail(mailOptions)
 }
 
 /**
@@ -66,20 +37,18 @@ const sendToken = async (
  * @param type the type of notice to send
  */
 const sendNotice = async (user: IUserSchema, type: 'delete' | 'recover') => {
-    const mailOptions: Mail.Options = {
-        from: `"TakeNote" ${constants.email.user}`,
-        to: user.email,
-        subject: type === 'delete'
-            ? `TakeNote Account Account Deletion - ${user.username}`
-            : `TakeNote Account Account Recovery - ${user.username}`,
-        text: type === 'delete'
-            ? 'Your account is scheduled for deletion. You can abort this process in the next 7 days.'
-            : 'Your account has been recovered, alongside all the data associated with it.',
-        html: type === 'delete'
-            ? 'Your account is scheduled for deletion. You can abort this process in the next 7 days.'
-            : 'Your account has been recovered, alongside all the data associated with it.'
-    }
-    const transporter = nodemailer.createTransport({
+    const mailTransport = makeMailTransport()
+    const noticeMailContent = makeNoticeMailContent(user, type === 'delete')
+    const mailOptions = makeMailOptions(user, noticeMailContent)
+
+    await mailTransport.sendMail(mailOptions)
+}
+
+/**
+ * Makes an object of type Mail, using the credentials in the constants.config file.
+ */
+const makeMailTransport = () => {
+    return nodemailer.createTransport({
         service: 'gmail',
         secure: false,
         auth: {
@@ -90,7 +59,85 @@ const sendNotice = async (user: IUserSchema, type: 'delete' | 'recover') => {
             rejectUnauthorized: false
         }
     })
-    await transporter.sendMail(mailOptions)
+}
+
+/**
+ * Makes a full URL with the data provided.
+ * @param data object with token and path props
+ */
+const makeCompleteURL = (data: {token: string, path: string}) => {
+    return url.format({
+        query: {
+            token: data.token
+        },
+        protocol: 'http', // TODO: change to https
+        host: constants.domain.baseDomain,
+        pathname: data.path
+    })
+}
+
+/**
+ * Makes an object of type Mail.Options.
+ * @param user object representing a user
+ * @param mailContent object with subject, text and html props to be included in the email
+ */
+const makeMailOptions = (user: IUserSchema, mailContent: {
+    subject: string,
+    text: string,
+    html: string
+}): Mail.Options => {
+    return {
+        from: `"TakeNote" ${constants.email.user}`,
+        to: user.email,
+        subject: mailContent.subject,
+        text: mailContent.text,
+        html: mailContent.html
+    }
+}
+
+/**
+ * Makes an object with subject, text and html props, representing the content of a token email.
+ * @param user object representing a user
+ * @param url the URL to be embedded in the message
+ * @param forConfirmatin true for an account confirmation message, false for a password reset message
+ */
+const makeTokenMailContent = (
+    user: IUserSchema,
+    url: string,
+    forConfirmation: boolean
+) => {
+    if (forConfirmation) {
+        return {
+            subject: `TakeNote Account Confirmation - ${user.username}`,
+            text: `Go to this URL to confirm your account: ${url}`,
+            html: `<h3>Click the button below to confirm your TakeNote account</h3><button><a href=${url} target="_blank">Confirm</a></button><p>Button not working? Go to this URL: ${url}</p>`
+        }
+    }
+    return {
+        subject: `TakeNote Account Password Reset - ${user.username}`,
+        text: `Go to this URL to reset your password: ${url}`,
+        html: `<h3>Click the button below to reset your TakeNote password</h3><button><a href=${url} target="_blank">Reset Password</a></button><p>Button not working? Go to this URL: ${url}</p>`
+    }
+}
+
+/**
+ * Makes an object with subject, text and html props, representing the content of a notice email.
+ * @param user object representing a user
+ * @param forDeletion true for a deletion notice, false for a recovery notice
+ */
+const makeNoticeMailContent = (user: IUserSchema, forDeletion: boolean) => {
+    if (forDeletion) {
+        return {
+            subject: `TakeNote Account Deletion - ${user.username}`,
+            text: 'Your account is scheduled for deletion. You can abort this process in the next 7 days.',
+            html: 'Your account is scheduled for deletion. You can abort this process in the next 7 days.'
+        }
+    }
+    return {
+        subject: `TakeNote Account Recovery - ${user.username}`,
+        text: 'Your account has been recovered, alongside all the data associated with it.',
+        html: 'Your account has been recovered, alongside all the data associated with it.'
+    }
 }
 
 export default { sendToken, sendNotice }
