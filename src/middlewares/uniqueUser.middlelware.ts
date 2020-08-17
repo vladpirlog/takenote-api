1bdc52db04a9bbaa58171ba2dbc73e0d5df6881b
Vlad Pirlog <avi.pirlog@gmail.com>
2020-08-17 18:18:25 +0300

    :sparkles: Add endpoint for checking credentials

--- src/middlewares/uniqueUser.middlelware.ts
+++ src/middlewares/uniqueUser.middlelware.ts
@@ -2,17 +2,21 @@ import { Request, Response, NextFunction } from 'express'
 import createResponse from '../utils/createResponse.util'
 import userQuery from '../queries/user.query'
 
-const checkUniqueUser = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const { username, email } = req.body
-    try {
-        const user = await userQuery.getByUsernameOrEmail(username, email)
-        if (user) return createResponse(res, 409, 'User already exists.')
-        return next()
-    } catch (err) { return next(err) }
+const checkUniqueUser = (isFinalMiddleware: boolean) => {
+    return async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { username, email } = req.body
+        try {
+            const user = await userQuery.getByUsernameOrEmail(username, email)
+            if (user) return createResponse(res, 409, 'User already exists.')
+            return isFinalMiddleware
+                ? createResponse(res, 200)
+                : next()
+        } catch (err) { return next(err) }
+    }
 }
 
 export default checkUniqueUser
