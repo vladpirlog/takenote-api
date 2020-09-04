2ff61f0495c0179113a5b08a13e1a0b585fcd0e4
Vlad Pirlog <avi.pirlog@gmail.com>
2020-09-05 00:47:07 +0300

    Improve queries for limits

---
+++ src/queries/limits.query.ts
@@ -0,0 +1,44 @@
+import Note, { INoteSchema } from '../models/Note'
+import { IUserSchema } from '../models/User'
+
+const note = (userID: IUserSchema['_id']) => {
+    return Note.find({ owner: userID }).count().exec()
+}
+
+const tag = async (
+    noteID: INoteSchema['_id'],
+    userID: IUserSchema['_id']
+): Promise<number | undefined> => {
+    const output = await Note.aggregate([
+        {
+            $match: { _id: noteID, owner: userID }
+        },
+        {
+            $project: { tagsLength: { $size: '$tags' } }
+        }
+    ]).exec()
+    return output[0].tagsLength
+}
+
+const permissionOrAttachment = (type: 'permissions' | 'attachments') => {
+    return async (
+        noteID: INoteSchema['_id'],
+        userID: IUserSchema['_id']
+    ): Promise<number | undefined> => {
+        const output = await Note.aggregate([
+            {
+                $match: { _id: noteID, owner: userID }
+            },
+            {
+                $project: { length: { $size: `$${type}` } }
+            }
+        ]).exec()
+        return output[0].length
+    }
+}
+
+export default {
+    note,
+    tag,
+    permissionOrAttachment
+}
