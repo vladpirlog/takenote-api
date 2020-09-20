c1f646b6e2648f6e13aceccf95b03f6ecb993962
Vlad Pirlog <avi.pirlog@gmail.com>
2020-09-20 16:29:04 +0300

    Validate request strings

--- src/middlewares/requestFieldsDefined.middleware.ts
+++ src/middlewares/requestFieldsDefined.middleware.ts
@@ -2,20 +2,21 @@ import { Request, Response, NextFunction } from 'express'
 import createResponse from '../utils/createResponse.util'
 import isPropertyOfObject from '../utils/isPropertyOfObject.util'
 import deleteFile from '../utils/deleteFile.util'
+import isString from '../utils/isString.util'
 
 /**
- * Higher order function for checking the existence of certain fields in a part of the request.
+ * Higher order function for checking if certain fields in a part of the request are strings.
  * @returns a middleware function
  * @param location the request property to make the check on
  * @param fields an array of params that must be defined
  */
 const requestFieldsDefined = (location: 'body' | 'params' | 'query', fields: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (fields.every(f => isPropertyOfObject(f, req[location]))) {
+        if (fields.every(f => isPropertyOfObject(f, req[location]) && isString(req[location][f]))) {
             return next()
         }
         if (req.files?.photo) deleteFile(req.files.photo)
-        return createResponse(res, 422, `Data missing from ${location}.`)
+        return createResponse(res, 422)
     }
 }
 
