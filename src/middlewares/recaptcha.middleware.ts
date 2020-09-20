114df5307293b92ee1f19483fa86628333d7089b
Vlad Pirlog <avi.pirlog@gmail.com>
2020-09-20 16:31:11 +0300

    :ambulance: Use recaptcha only in prod

--- src/middlewares/recaptcha.middleware.ts
+++ src/middlewares/recaptcha.middleware.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
+import constants from '../config/constants.config'
 import createResponse from '../utils/createResponse.util'
 import recaptchaUtil from '../utils/recaptcha.util'
 
 const recaptcha = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        if (constants.nodeEnv !== 'production') return next()
+
         const { 'g-recaptcha-response': recaptchaCode } = req.body
         if (!recaptchaCode) return createResponse(res, 422, 'ReCAPTCHA code missing from body.')
 
