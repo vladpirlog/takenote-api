4780db1e1aea5767b1a19c7b8e62327897acc80b
Vlad Pirlog <avi.pirlog@gmail.com>
2020-09-21 00:07:18 +0300

    :white_check_mark: Update comments

--- src/middlewares/send404.util.ts
+++ src/middlewares/send404.util.ts
@@ -3,7 +3,6 @@ import createResponse from '../utils/createResponse.util'
 
 /**
  * Middleware function that sends a 404 response to all the request that didn't match any other route.
- * It's the last function in the middleware stack.
  */
 export default function (req: Request, res: Response) {
     return createResponse(res)
