b20378afac8b708781a2e96e5651fd55609a5c77
Vlad Pirlog <avi.pirlog@gmail.com>
2020-10-18 13:09:31 +0300

    :sparkles: Add logging middleware

---
+++ src/config/moesif.config.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import getAuthUser from '../utils/getAuthUser.util'
+import constants from './constants.config'
+const moesif = require('moesif-nodejs')
+
+const moesifLoggingMiddleware = moesif({
+    applicationId: constants.moesif.apiKey,
+    logBody: false,
+    identifyUser: (req: Request, res: Response) => getAuthUser(res)?._id,
+    skip: (req: Request) => req.path === '/'
+})
+
+export default moesifLoggingMiddleware
