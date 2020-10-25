66f9d18a795bac3c5a41bed3e07412a9f2847429
Vlad Pirlog <avi.pirlog@gmail.com>
2020-10-25 17:18:02 +0200

    Fix timezone in getUnixTime tests

--- test/getUnixTime.util.test.ts
+++ test/getUnixTime.util.test.ts
@@ -3,9 +3,8 @@ import getUnixTime from '../src/utils/getUnixTime.util'
 describe('unix time testing', () => {
     const inputAndOutput: [Date | undefined, number][] = [
         [new Date(), Math.floor(new Date().getTime() / 1000)],
-        [new Date('October 21, 2020 23:13:28'), 1603311208],
-        [new Date('2020-10-21T20:13:28+00:00'), 1603311208],
-        [new Date(2020, 9, 21, 23, 13, 28), 1603311208]
+        [new Date('Wed, 21 Oct 2020 20:13:28 +0000'), 1603311208],
+        [new Date('2020-10-21T20:13:28+00:00'), 1603311208]
     ]
 
     test.each(inputAndOutput)('%j should be %j', (input, output) => {
