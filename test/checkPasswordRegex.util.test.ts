65bb4bf6ed09eabd6058ddb2af87b70848266134
Vlad Pirlog <avi.pirlog@gmail.com>
2020-10-26 12:59:45 +0200

    :fire: Remove checkRegex function and tests

--- test/checkPasswordRegex.util.test.ts
+++ test/checkPasswordRegex.util.test.ts
@@ -1,25 +1,30 @@
-import checkRegex from '../src/utils/checkRegex.util'
 import constants from '../src/config/constants.config'
 
 describe('password RegEx testing', () => {
     const acceptedStrings = [
         'Abbccdd1!',
-        ['Qwerty1!', 'Asdfgh0?']
+        'Qwerty1!',
+        '9KsQ@G38',
+        '*R$3jzZ@4O8%1HtV',
+        '*cDsAsKpHX1ytk714NAgaV2da3UEn$RP@e8$ZjrD'
     ]
 
     const rejectedStrings = [
+        '',
         'lsls',
         'abcd',
         'Test1!',
-        ['qwerty1!', 'Asdfgh0?'],
-        ['qwerty1!', 'asdfgh0_']
+        'qwerty1!',
+        'qwerty1!-',
+        'asdfgh0_',
+        '*cDsAsKpHX1ytk714NAgaV2da3UEn$RP@e8$ZjrDD'
     ]
 
     test.each(acceptedStrings)('%j should return true', (s) => {
-        expect(checkRegex(constants.regex.password, s)).toBeTruthy()
+        expect(constants.regex.password.test(s)).toBeTruthy()
     })
 
     test.each(rejectedStrings)('%j should return false', (s) => {
-        expect(checkRegex(constants.regex.password, s)).toBeFalsy()
+        expect(constants.regex.password.test(s)).toBeFalsy()
     })
 })
--- test/checkUsernameRegex.util.test.ts
+++ test/checkUsernameRegex.util.test.ts
@@ -1,26 +1,31 @@
-import checkRegex from '../src/utils/checkRegex.util'
 import constants from '../src/config/constants.config'
 
 describe('username RegEx testing', () => {
     const acceptedStrings = [
         'salut',
-        ['AAZZ0-9_'],
-        ['qwerty', 'uiop00000']
+        'AAZZ0-9_',
+        'qwerty',
+        'uiop00000',
+        'lsdgng979-_673jdsgl6',
+        'lala-eeligg',
+        'ppppp'
     ]
 
     const rejectedStrings = [
         'a',
+        '',
         '-_]]][',
         'jlsdfjdksfdsjgerglkjgoigj',
-        ['ppppp', 'aaaaaajsgjsg ghe'],
-        ['..', 'lala-eeligg']
+        'aaaaaajsgjsg ghe',
+        '..',
+        'lsdgng979-_673jdsgl6r'
     ]
 
     test.each(acceptedStrings)('%j should return true', (s) => {
-        expect(checkRegex(constants.regex.username, s)).toBeTruthy()
+        expect(constants.regex.username.test(s)).toBeTruthy()
     })
 
     test.each(rejectedStrings)('%j should return false', (s) => {
-        expect(checkRegex(constants.regex.username, s)).toBeFalsy()
+        expect(constants.regex.username.test(s)).toBeFalsy()
     })
 })
