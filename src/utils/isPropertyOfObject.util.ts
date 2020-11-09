66b0a8a7a09258b621281d96c35e5fc089af8a1e
Vlad Pirlog <avi.pirlog@gmail.com>
2020-11-10 00:05:45 +0200

    Remove unnecessary utils and tests

--- src/utils/isPropertyOfObject.util.ts
+++
@@ -1,5 +0,0 @@
-const isPropertyOfObject = (property: string, obj: Object) => {
-    return Object.keys(obj).includes(property)
-}
-
-export default isPropertyOfObject
--- src/utils/isString.util.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Checks if the given argument is a string.
- * @param val object to be checked
- */
-const isString = (val: any) => {
-    return typeof val === 'string' || val instanceof String
-}
-
-export default isString
