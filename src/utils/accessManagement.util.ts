35de5298cf65f8d4d6dc7d4ed9c9f082f562a19e
Vlad Pirlog <avi.pirlog@gmail.com>
2020-11-10 00:29:56 +0200

    Change roles and permissions

--- src/utils/accessManagement.util.ts
+++ src/utils/accessManagement.util.ts
@@ -1,99 +1,108 @@
-/**
- * - OWNER -> can do anything to a note
- * - PRIMARY_COLLABORATOR -> OWNER, except delete the note and comments
- * - SECONDARY_COLLABORATOR -> PRIMARY_COLLABORATOR, except editing collaborators or sharing
- * - OBSERVER -> can only view note, comments, attachments, collaborators and sharing
- */
-export enum NoteRole {
-    OWNER = 'owner',
-    PRIMARY_COLLABORATOR = 'primary_collaborator',
-    SECONDARY_COLLABORATOR = 'secondary_collaborator',
-    OBSERVER = 'observer',
-}
-
-export enum NotePermission {
-    NOTE_EDIT_COMMON_PROPERTIES = 'note_edit_common_properties',
-    NOTE_EDIT_PERSONAL_PROPERTIES = 'note_edit_personal_properties',
-    NOTE_VIEW = 'note_view',
-    NOTE_DELETE = 'note_delete',
-
-    ATTACHMENT_ADD = 'attachment_add',
-    ATTACHMENT_DELETE = 'attachment_delete',
-    ATTACHMENT_VIEW = 'attachment_view',
-
-    COMMENT_ADD = 'comment_add',
-    COMMENT_DELETE = 'comment_delete',
-    COMMENT_VIEW = 'comment_view',
-
-    COMMENTS_CHANGE_STATE = 'comments_change_state',
-
-    COLLABORATOR_ADD = 'collaborator_add',
-    COLLABORATOR_DELETE = 'collaborator_delete',
-    COLLABORATOR_VIEW = 'collaborator_view',
-
-    SHARING_EDIT = 'sharing_edit',
-    SHARING_VIEW = 'sharing_view'
-}
+import { Permission } from '../enums/Permission.enum'
+import { Role } from '../enums/Role.enum'
 
 export const RoleToPermissionsMap = {
-    owner: [
-        NotePermission.NOTE_EDIT_COMMON_PROPERTIES,
-        NotePermission.NOTE_EDIT_PERSONAL_PROPERTIES,
-        NotePermission.NOTE_VIEW,
-        NotePermission.NOTE_DELETE,
-        NotePermission.ATTACHMENT_ADD,
-        NotePermission.ATTACHMENT_DELETE,
-        NotePermission.ATTACHMENT_VIEW,
-        NotePermission.COMMENT_ADD,
-        NotePermission.COMMENT_DELETE,
-        NotePermission.COMMENT_VIEW,
-        NotePermission.COMMENTS_CHANGE_STATE,
-        NotePermission.COLLABORATOR_ADD,
-        NotePermission.COLLABORATOR_DELETE,
-        NotePermission.COLLABORATOR_VIEW,
-        NotePermission.SHARING_EDIT,
-        NotePermission.SHARING_VIEW
-    ],
-    primary_collaborator: [
-        NotePermission.NOTE_EDIT_COMMON_PROPERTIES,
-        NotePermission.NOTE_EDIT_PERSONAL_PROPERTIES,
-        NotePermission.NOTE_VIEW,
-        NotePermission.COMMENT_ADD,
-        NotePermission.COMMENT_VIEW,
-        NotePermission.COMMENTS_CHANGE_STATE,
-        NotePermission.ATTACHMENT_ADD,
-        NotePermission.ATTACHMENT_DELETE,
-        NotePermission.ATTACHMENT_VIEW,
-        NotePermission.COLLABORATOR_VIEW,
-        NotePermission.COLLABORATOR_ADD,
-        NotePermission.COLLABORATOR_DELETE,
-        NotePermission.SHARING_EDIT,
-        NotePermission.SHARING_VIEW
-    ],
-    secondary_collaborator: [
-        NotePermission.NOTE_EDIT_COMMON_PROPERTIES,
-        NotePermission.NOTE_EDIT_PERSONAL_PROPERTIES,
-        NotePermission.NOTE_VIEW,
-        NotePermission.COMMENT_ADD,
-        NotePermission.COMMENT_VIEW,
-        NotePermission.ATTACHMENT_ADD,
-        NotePermission.ATTACHMENT_DELETE,
-        NotePermission.ATTACHMENT_VIEW,
-        NotePermission.COLLABORATOR_VIEW,
-        NotePermission.SHARING_VIEW
-    ],
-    observer: [
-        NotePermission.NOTE_EDIT_PERSONAL_PROPERTIES,
-        NotePermission.NOTE_VIEW,
-        NotePermission.ATTACHMENT_VIEW,
-        NotePermission.COLLABORATOR_VIEW,
-        NotePermission.COMMENT_VIEW,
-        NotePermission.SHARING_VIEW
-    ]
+    owner: {
+        note: [
+            Permission.NOTE_EDIT_COMMON_PROPERTIES,
+            Permission.NOTE_EDIT_PERSONAL_PROPERTIES,
+            Permission.NOTE_VIEW,
+            Permission.NOTE_DELETE,
+            Permission.NOTE_MOVE,
+            Permission.NOTE_ATTACHMENT_ADD,
+            Permission.NOTE_ATTACHMENT_DELETE,
+            Permission.NOTE_ATTACHMENT_VIEW,
+            Permission.NOTE_COMMENT_ADD,
+            Permission.NOTE_COMMENT_DELETE,
+            Permission.NOTE_COMMENT_VIEW,
+            Permission.NOTE_COMMENTS_CHANGE_STATE,
+            Permission.NOTE_COLLABORATOR_ADD,
+            Permission.NOTE_COLLABORATOR_DELETE,
+            Permission.NOTE_COLLABORATOR_VIEW,
+            Permission.NOTE_SHARING_EDIT,
+            Permission.NOTE_SHARING_VIEW
+        ],
+        notepad: [
+            Permission.NOTEPAD_VIEW,
+            Permission.NOTEPAD_DELETE,
+            Permission.NOTEPAD_ADD_NOTES,
+            Permission.NOTEPAD_EDIT_COMMON_PROPERTIES,
+            Permission.NOTEPAD_COLLABORATOR_ADD,
+            Permission.NOTEPAD_COLLABORATOR_DELETE,
+            Permission.NOTEPAD_COLLABORATOR_VIEW,
+            Permission.NOTEPAD_SHARING_EDIT,
+            Permission.NOTEPAD_SHARING_VIEW
+        ]
+    },
+    primary_collaborator: {
+        note: [
+            Permission.NOTE_EDIT_COMMON_PROPERTIES,
+            Permission.NOTE_EDIT_PERSONAL_PROPERTIES,
+            Permission.NOTE_VIEW,
+            Permission.NOTE_COMMENT_ADD,
+            Permission.NOTE_COMMENT_VIEW,
+            Permission.NOTE_COMMENTS_CHANGE_STATE,
+            Permission.NOTE_ATTACHMENT_ADD,
+            Permission.NOTE_ATTACHMENT_DELETE,
+            Permission.NOTE_ATTACHMENT_VIEW,
+            Permission.NOTE_COLLABORATOR_VIEW,
+            Permission.NOTE_COLLABORATOR_ADD,
+            Permission.NOTE_COLLABORATOR_DELETE,
+            Permission.NOTE_SHARING_EDIT,
+            Permission.NOTE_SHARING_VIEW
+        ],
+        notepad: [
+            Permission.NOTEPAD_VIEW,
+            Permission.NOTEPAD_ADD_NOTES,
+            Permission.NOTEPAD_EDIT_COMMON_PROPERTIES,
+            Permission.NOTEPAD_SHARING_VIEW,
+            Permission.NOTEPAD_SHARING_EDIT,
+            Permission.NOTEPAD_COLLABORATOR_ADD,
+            Permission.NOTEPAD_COLLABORATOR_DELETE,
+            Permission.NOTEPAD_COLLABORATOR_VIEW
+        ]
+    },
+    secondary_collaborator: {
+        note: [
+            Permission.NOTE_EDIT_COMMON_PROPERTIES,
+            Permission.NOTE_EDIT_PERSONAL_PROPERTIES,
+            Permission.NOTE_VIEW,
+            Permission.NOTE_COMMENT_ADD,
+            Permission.NOTE_COMMENT_VIEW,
+            Permission.NOTE_ATTACHMENT_ADD,
+            Permission.NOTE_ATTACHMENT_DELETE,
+            Permission.NOTE_ATTACHMENT_VIEW,
+            Permission.NOTE_COLLABORATOR_VIEW,
+            Permission.NOTE_SHARING_VIEW
+        ],
+        notepad: [
+            Permission.NOTEPAD_VIEW,
+            Permission.NOTEPAD_SHARING_VIEW,
+            Permission.NOTEPAD_COLLABORATOR_VIEW
+        ]
+    },
+    observer: {
+        note: [
+            Permission.NOTE_EDIT_PERSONAL_PROPERTIES,
+            Permission.NOTE_VIEW,
+            Permission.NOTE_ATTACHMENT_VIEW,
+            Permission.NOTE_COLLABORATOR_VIEW,
+            Permission.NOTE_COMMENT_VIEW,
+            Permission.NOTE_SHARING_VIEW
+        ],
+        notepad: [
+            Permission.NOTEPAD_VIEW,
+            Permission.NOTEPAD_SHARING_VIEW,
+            Permission.NOTEPAD_COLLABORATOR_VIEW
+        ]
+    }
 }
 
-export const getPermissionsFromRoles = (roles: NoteRole[]) => {
-    const permissionsSet = new Set<NotePermission>()
-    roles.forEach(r => RoleToPermissionsMap[r].forEach(p => permissionsSet.add(p)))
+export const getPermissionsFromRoles = (
+    entityType: 'note' | 'notepad',
+    roles: Role[]
+) => {
+    const permissionsSet = new Set<Permission>()
+    roles.forEach(r => RoleToPermissionsMap[r][entityType].forEach(p => permissionsSet.add(p)))
     return Array.from(permissionsSet)
 }
