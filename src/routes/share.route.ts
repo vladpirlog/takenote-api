f2c15dc55dfe8d4b3434176c3c6252c8b5ccbcb5
Vlad Pirlog <avi.pirlog@gmail.com>
2020-11-10 13:31:00 +0200

    :art: Merge sharing controllers

--- src/routes/share.route.ts
+++ src/routes/share.route.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express'
-import noteShareController from '../controllers/note.share.controller'
-import notepadShareController from '../controllers/notepad.share.controller'
+import shareController from '../controllers/share.controller'
 
 const router = Router()
 
 /**
  * GET a shared note using the share code; no authentication required
  */
-router.get('/note/:code', noteShareController.getNote)
+router.get('/note/:code', shareController.getNote)
 
 /**
  * GET a shared notepad using the share code; no authentication required
  */
-router.get('/notepad/:code', notepadShareController.getNotepad)
+router.get('/notepad/:code', shareController.getNotepad)
 
 export default router
